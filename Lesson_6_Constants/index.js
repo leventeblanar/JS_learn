8154566c5058a22ced7186ed3634a24ed4ec86b8
leventeblanar <blanar.levente@gmail.com>
2024-12-23 20:53:34 +0100

    Lesson 6 and 7

--- Lesson_6_Constants/index.js
+++ Lesson_6_Constants/index.js
@@ -1,5 +1,6 @@
 // const = a variable that can't be changed
 
+/*
 const PI = 3.14159;
 let radius;
 let circumference;
@@ -10,3 +11,15 @@ radius = Number(radius);
 circumference = 2 * pi * radius;
 
 console.log(circumference);
+*/
+
+const PI = 3.14159;
+let radius;
+let circumference;
+
+document.getElementById("mySubmit").onclick = function(){
+    radius = document.getElementById("myText").value;
+    radius = Number(radius);
+    circumference = 2 * PI * radius;
+    document.getElementById(`myH3`).textContent = circumference;
+}
